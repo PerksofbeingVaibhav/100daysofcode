c0e72cfedb0de058f3bcdbcb9de0458ed50c035e
Vaibhav Chaudhary <83056597+PerksofbeingVaibhav@users.noreply.github.com>
2022-04-26 20:26:55 +0530

    day 23

---
+++ CP/GEEKSFORGEEKS(SDE SHEET)/POTD.cpp
@@ -0,0 +1,22 @@
+void zigZag(int arr[], int n) {
+	    // code here
+	    bool flag=true;
+	    for(int i=0;i<=n-2;i++)
+	    {
+	        if(flag)
+	        {
+	            if(arr[i]>arr[i+1])
+	            {
+	                swap(arr[i],arr[i+1]);
+	            }
+	        }else
+	        {
+	            if(arr[i]<arr[i+1])
+	            {
+	                swap(arr[i],arr[i+1]);
+	            }
+	        }
+	        flag=!flag;
+	    }
+	    
+	}
