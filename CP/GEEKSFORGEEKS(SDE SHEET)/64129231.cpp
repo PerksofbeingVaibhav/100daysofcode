f1630e0d70307fe832658fcb7d4b9729edad1e42
Vaibhav Chaudhary <83056597+PerksofbeingVaibhav@users.noreply.github.com>
2022-05-03 23:42:42 +0530

    day 30

---
+++ CP/GEEKSFORGEEKS(SDE SHEET)/64129231.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tc;
+    cin>>tc;
+    while(tc--){
+        int n;
+        cin>>n;
+        if(n==2 || n==3){
+            cout<< n-1<<endl;
+        }
+        else {
+            cout<<n<<endl;
+        }
+    
+    }
+	// your code goes here
+	return 0;
+}
